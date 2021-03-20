b2ce7a677d12124beba5312cd44dbdccd4d54e6e
DougZheng <739168993@qq.com>
2021-03-20 10:39:58 +0800

    ready for baseline

--- meterials/SDK/SDK_C++/build_and_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT=$(readlink -f "$0")
-BASEDIR=$(dirname "$SCRIPT")
-cd $BASEDIR
-
-sh build.sh
-cd bin
-./CodeCraft-2021
--- meterials/SDK/SDK_python/build_and_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT=$(readlink -f "$0")
-BASEDIR=$(dirname "$SCRIPT")
-cd $BASEDIR
-
-cd CodeCraft-2021
-python src/CodeCraft-2021.py
